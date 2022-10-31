33ac8ebaca1cb054cbf45d66c93838a81ede9f14
ned <edodangelis@yahoo.it>
2022-10-31 11:59:53 +0100

    Initial Commit

---
+++ Stats/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Stats
+//
+//  Created by ned on 31/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stats/StatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsApp.swift
+//  Stats
+//
+//  Created by ned on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct StatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
